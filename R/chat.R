ca8f49411bad9a7b5132cf5df2d974f5c897a600
Garrick Aden-Buie <garrick@adenbuie.com>
2025-05-14 10:55:06 -0400

    feat: Don't crash app with an error in `chat_append_stream()`

--- R/chat.R
+++ R/chat.R
@@ -423,13 +423,22 @@ chat_append_stream <- function(
       operation = "append",
       session = session
     )
+    rlang::warn(
+      sprintf(
+        "ERROR: An error occurred in `chat_append_stream(id=\"%s\")`",
+        id
+      ),
+      parent = reason
+    )
+    # ...but rethrow the error as a silent error, so the caller can also handle
+    # it if they want, but it won't bring down the app. Note that we want to
+    # return a rejected promise (so the caller can see the error) that was
+    # already handled (so there's no "unhandled promise error" warning if the
+    # caller chooses not to do anything with it).
+    class(reason) <- c("shiny.silent.error", class(reason))
+    cnd_signal(reason)
   })
-  # ...but also return it, so the caller can also handle it if they want. Note
-  # that we're not returning the result of `promises::catch`; we want to return
-  # a rejected promise (so the caller can see the error) that was already
-  # handled (so there's no "unhandled promise error" warning if the caller
-  # chooses not to do anything with it).
-  result
+  # result
 }
 
 utils:::globalVariables(c("generator_env", "exits", "yield"))
