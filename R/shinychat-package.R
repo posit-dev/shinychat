bf3233fb43ef63a6cccbbfd973f73b0756cc7d17
Barret Schloerke <barret@posit.co>
2025-05-20 14:41:22 -0400

    Add S7

--- R/shinychat-package.R
+++ R/shinychat-package.R
@@ -8,6 +8,10 @@
 ## usethis namespace: end
 NULL
 
+# enable usage of <S7_object>@name in package code
+#' @rawNamespace if (getRversion() < "4.3.0") importFrom("S7", "@")
+NULL
+
 ignore_unused_imports <- function() {
   jsonlite::fromJSON
   fastmap::fastqueue
