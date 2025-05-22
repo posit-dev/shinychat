15bcda5a21e325a822916eea7b6df87c459cb68e
Barret Schloerke <barret@posit.co>
2025-05-22 15:15:02 -0400

    Update client_state.R

--- R/client_state.R
+++ R/client_state.R
@@ -125,3 +125,9 @@ method(client_set_ui, S7::new_S3_class(c("Chat", "R6"))) <-
       )
     })
   }
+
+
+# Used to avoid R CMD check NOTE about unused imports
+`_ignore` <- function() {
+  base64enc::base64encode
+}
