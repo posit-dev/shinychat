657015b3a0e59fa12aa42a4058e2fc265013eff2
Karan Gathani <karan.gathani@posit.co>
2025-06-18 13:34:13 +0530

    add missing structured_output for llm

---
+++ pkg-py/tests/playwright/chat/llm_package/structured_output/app.py
@@ -0,0 +1,42 @@
+import llm
+from dotenv import load_dotenv
+from shiny.express import ui
+from pydantic import BaseModel
+
+
+# Load environment variables from .env file
+_ = load_dotenv()
+
+
+class Dog(BaseModel):
+    name: str
+    age: int
+
+model = llm.get_model("gpt-4o-mini")
+
+
+model.system_prompt = "You are a helpful assistant."
+
+ui.page_opts(
+    title="Hello LLM Chat",
+    fillable=True,
+    fillable_mobile=True,
+)
+
+
+chat = ui.Chat(
+    id="chat",
+    messages=["Hello! I am a bot using `llm` package with OpenAI. How can I help?"],
+)
+chat.ui()
+
+
+@chat.on_user_submit
+async def handle_user_input(user_input: str):
+    response = model.prompt(user_input, stream=True, schema=Dog)
+
+    async def stream_generator():
+        for chunk in response:
+            yield chunk
+
+    await chat.append_message_stream(stream_generator())
