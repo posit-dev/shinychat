e2ccbcba54ca9e6d2b1510fe9588bb02f8c68b41
Garrick Aden-Buie <garrick@adenbuie.com>
2025-07-31 13:23:42 -0400

    chore: re-implement toggle icon using svg
    For consistent widths, otherwise divs look differently by browser and font-size

--- js/src/chat/chat-tools.ts
+++ js/src/chat/chat-tools.ts
@@ -82,7 +82,7 @@ class ShinyToolCard extends LitElement {
           aria-controls="${contentId}"
         >
           ${headerContent}
-          <div class="collapse-indicator"></div>
+          <div class="collapse-indicator">${unsafeHTML(ICONS.plus)}</div>
         </button>
         <div
           class="card-body bslib-gap-spacing html-fill-item html-fill-container${this
@@ -282,6 +282,10 @@ const ICONS = {
   wrenchAdjustable: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wrench-adjustable" viewBox="0 0 16 16">
   <path d="M16 4.5a4.5 4.5 0 0 1-1.703 3.526L13 5l2.959-1.11q.04.3.041.61"/>
   <path d="M11.5 9c.653 0 1.273-.139 1.833-.39L12 5.5 11 3l3.826-1.53A4.5 4.5 0 0 0 7.29 6.092l-6.116 5.096a2.583 2.583 0 1 0 3.638 3.638L9.908 8.71A4.5 4.5 0 0 0 11.5 9m-1.292-4.361-.596.893.809-.27a.25.25 0 0 1 .287.377l-.596.893.809-.27.158.475-1.5.5a.25.25 0 0 1-.287-.376l.596-.893-.809.27a.25.25 0 0 1-.287-.377l.596-.893-.809.27-.158-.475 1.5-.5a.25.25 0 0 1 .287.376M3 14a1 1 0 1 1 0-2 1 1 0 0 1 0 2"/>
+</svg>`,
+  plus: `<svg xmlns="http://www.w3.org/2000/svg" width="10px" height="10px" viewBox="4 4 12 12" fill="none">
+  <path class="horizontal" d="M5 11C4.44772 11 4 10.5523 4 10C4 9.44772 4.44772 9 5 9H15C15.5523 9 16 9.44772 16 10C16 10.5523 15.5523 11 15 11H5Z" fill="currentColor"/>
+  <path class="vertical" d="M9 5C9 4.44772 9.44772 4 10 4C10.5523 4 11 4.44772 11 5V15C11 15.5523 10.5523 16 10 16C9.44772 16 9 15.5523 9 15V5Z" fill="currentColor"/>
 </svg>`,
 }
 
