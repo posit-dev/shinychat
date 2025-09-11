36517aab2eb35b34ac712d2ab89194bbd7cc14e6
Carson <cpsievert1@gmail.com>
2025-09-11 15:05:17 -0500

    fix(pkg-py): allow ToolResultDisplay to be imported without pydantic

--- pkg-py/src/shinychat/types/__init__.py
+++ pkg-py/src/shinychat/types/__init__.py
@@ -1,7 +1,19 @@
 from .._chat import ChatMessage, ChatMessageDict
-from .._chat_normalize_chatlas import ToolResultDisplay
 
-ToolResultDisplay.model_rebuild()
+try:
+    from .._chat_normalize_chatlas import ToolResultDisplay
+
+    ToolResultDisplay.model_rebuild()
+except ImportError:
+
+    class MockToolResultDisplay:
+        def __init__(self, *args, **kwargs):
+            raise ImportError(
+                "ToolResultDisplay requires the 'chatlas' package to be installed."
+            )
+
+    ToolResultDisplay = MockToolResultDisplay
+
 
 __all__ = [
     "ChatMessage",
