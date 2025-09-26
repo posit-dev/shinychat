b942ec31b158bf6207ed5b69425d505062dc3f73
Carson <cpsievert1@gmail.com>
2025-09-26 09:46:21 -0500

    Always morph the DOM when content has changed

--- js/src/markdown-stream/incremental-html-directive.ts
+++ js/src/markdown-stream/incremental-html-directive.ts
@@ -1,10 +1,11 @@
 import {
+  directive,
+  Directive,
   DirectiveParameters,
   PartInfo,
   PartType,
   ChildPart,
 } from "lit-html/directive.js"
-import { AsyncDirective, directive } from "lit/async-directive.js"
 import { noChange, html } from "lit-html"
 import { LitElement } from "lit"
 import { unsafeHTML } from "lit-html/directives/unsafe-html.js"
@@ -15,48 +16,8 @@ import type { ContentType } from "./markdown-renderer.js"
 import { createSVGIcon, throttle } from "../utils/_utils"
 import { findInnermostStreamingElement } from "./utils.js"
 
-// SVG dot to indicate content is currently streaming
-const SVG_DOT_CLASS = "markdown-stream-dot"
-const SVG_DOT = createSVGIcon(
-  `<svg width="12" height="12" xmlns="http://www.w3.org/2000/svg" class="${SVG_DOT_CLASS}" style="margin-left:.25em;margin-top:-.25em"><circle cx="6" cy="6" r="6"/></svg>`,
-)
-
-function removeStreamingDot(htmlContainer: HTMLElement): void {
-  htmlContainer.querySelector(`svg.${SVG_DOT_CLASS}`)?.remove()
-}
-
-function appendStreamingDot(htmlContainer: HTMLElement): void {
-  // When message starts, we may still be waiting for the first meaningful
-  // chunk, and in that case, the message icon should indicate progress
-  if (htmlContainer.children.length === 0) {
-    return
-  }
-  // Tool requests already indicate progress
-  // TODO: could also be done via CSS?
-  if (
-    htmlContainer?.lastElementChild?.tagName.toLowerCase() ===
-    "shiny-tool-request"
-  ) {
-    return
-  }
-  const innerEl = findInnermostStreamingElement(htmlContainer)
-  innerEl.appendChild(SVG_DOT)
-}
-
-// Wrap content in a <div> so that we can morph that div instead
-// of the lit container itself
-const CONTENT_CONTAINER_CLASS = "content-container"
-function renderToHTMLContainer(content: string, contentType: ContentType) {
-  return `<div class="${CONTENT_CONTAINER_CLASS}">
-      ${renderToHTML(content, contentType)}
-    </div>`
-}
-// Given the (host) lit Element, get the actual HTML container
-function getHTMLContainer(container: LitElement): HTMLElement {
-  return container.querySelector(`.${CONTENT_CONTAINER_CLASS}`) as HTMLElement
-}
-
-class IncrementalHTMLStream extends AsyncDirective {
+class IncrementalHTMLStream extends Directive {
+  private hasRendered = false
   // The content string from the previous render
   private lastContent: string = ""
   // The content type of the previous render
@@ -90,26 +51,27 @@ class IncrementalHTMLStream extends AsyncDirective {
       )
     }
 
-    // Do a full (re)-render if the content type has changed or if the
-    // new content doesn't start with the previous content
-    if (
-      contentType !== this.lastContentType ||
-      !content.startsWith(this.lastContent)
-    ) {
-      const html = renderToHTMLContainer(content, contentType)
-      this.lastContent = content
-      this.lastContentType = contentType
-      // TODO: This is technically a problem for MarkdownStream() since there
-      // will be no progress indication when the stream first starts
-      return unsafeHTML(html)
+    if (!this.hasRendered) {
+      this.hasRendered = true
+      if (streaming && !content) {
+        return html`${unsafeHTML(renderToHTMLContainer(SVG_DOT, "html"))}`
+      } else {
+        return html`${unsafeHTML(renderToHTMLContainer(content, contentType))}`
+      }
     }
 
-    if (content !== this.lastContent) {
-      // TODO: prevent a scenario of a re-render occurring in-between throttled updates
-      this._doIncrementalUpdate(container, content, contentType)
-      this.lastContent = content
-      this.lastContentType = contentType
+    // TODO: this is a workaround for a stream going from true -> false,
+    // but the content hasn't changed (because the content updated callbacks don't fire)
+    // We might want to rethink this logic so we don't have to track lastContent/lastContentType,
+    // possibly by only using this directive when streaming is relevant
+    if (content === this.lastContent && contentType === this.lastContentType) {
+      removeStreamingDot(getHTMLContainer(container))
+      return noChange
     }
+
+    this._doIncrementalUpdate(container, content, contentType)
+    this.lastContent = content
+    this.lastContentType = contentType
 
     // After morphing add/remove the streaming dot
     const htmlContainer = getHTMLContainer(container)
@@ -120,12 +82,6 @@ class IncrementalHTMLStream extends AsyncDirective {
     }
 
     return noChange
-  }
-
-  // Reset state when directive is disconnected
-  disconnected() {
-    this.lastContent = ""
-    this.lastContentType = null
   }
 
   // TODO: throttle the update?
@@ -153,6 +109,46 @@ class IncrementalHTMLStream extends AsyncDirective {
       },
     })
   }
+}
+
+// Wrap content in a <div> so that we can morph that div instead
+// of the lit container itself
+const CONTENT_CONTAINER_CLASS = "content-container"
+function renderToHTMLContainer(content: string, contentType: ContentType) {
+  return `<div class="${CONTENT_CONTAINER_CLASS}">
+      ${renderToHTML(content, contentType)}
+    </div>`
+}
+// Given the (host) lit Element, get the actual HTML container
+function getHTMLContainer(container: LitElement): HTMLElement {
+  return container.querySelector(`.${CONTENT_CONTAINER_CLASS}`) as HTMLElement
+}
+
+// SVG dot to indicate content is currently streaming
+const SVG_DOT_CLASS = "markdown-stream-dot"
+const SVG_DOT = `<svg width="12" height="12" xmlns="http://www.w3.org/2000/svg" class="${SVG_DOT_CLASS}" style="margin-left:.25em;margin-top:-.25em"><circle cx="6" cy="6" r="6"/></svg>`
+const SVG_DOT_EL = createSVGIcon(SVG_DOT)
+
+function removeStreamingDot(htmlContainer: HTMLElement): void {
+  htmlContainer.querySelector(`svg.${SVG_DOT_CLASS}`)?.remove()
+}
+
+function appendStreamingDot(htmlContainer: HTMLElement): void {
+  // When message starts, we may still be waiting for the first meaningful
+  // chunk, and in that case, the message icon should indicate progress
+  if (htmlContainer.children.length === 0) {
+    return
+  }
+  // Tool requests already indicate progress
+  // TODO: could also be done via CSS?
+  if (
+    htmlContainer?.lastElementChild?.tagName.toLowerCase() ===
+    "shiny-tool-request"
+  ) {
+    return
+  }
+  const innerEl = findInnermostStreamingElement(htmlContainer)
+  innerEl.appendChild(SVG_DOT_EL)
 }
 
 export const incrementalHTMLStream = directive(IncrementalHTMLStream)
