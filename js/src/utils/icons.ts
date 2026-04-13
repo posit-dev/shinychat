c21dd47a2b512aa40c4ac37455ea4e71db1eeedc
Garrick Aden-Buie <garrick@adenbuie.com>
2026-04-13 14:05:26 -0400

    fix(a11y): improve accessibility of scroll-to-bottom button
    - Add aria-hidden="true" to chevronDown SVG icon
    - Add prefers-reduced-motion media query to disable spin animation
    - Add :focus-visible outline for keyboard users
    - Increase button padding from 6px to 8px for better target size (32x32)
    - Use overflow:clip instead of overflow:hidden on wrapper to avoid
      clipping focus rings

--- js/src/utils/icons.ts
+++ js/src/utils/icons.ts
@@ -32,4 +32,4 @@ export const fullscreenEnter = `<svg xmlns="http://www.w3.org/2000/svg" viewBox=
 
 export const xLg = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16"><path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/></svg>`
 
-export const chevronDown = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/></svg>`
+export const chevronDown = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16" aria-hidden="true"><path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/></svg>`
