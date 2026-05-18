abad234fe4b0a22a1fca0ce16e8b9ba47dcdee6f
Garrick Aden-Buie <garrick@adenbuie.com>
2026-05-18 17:24:47 -0400

    refactor(py): move ChatGreeting export to types module

--- pkg-py/src/shinychat/__init__.py
+++ pkg-py/src/shinychat/__init__.py
@@ -1,10 +1,9 @@
-from ._chat import Chat, ChatGreeting, chat_greeting, chat_ui
+from ._chat import Chat, chat_greeting, chat_ui
 from ._chat_normalize import message_content, message_content_chunk
 from ._markdown_stream import MarkdownStream, output_markdown_stream
 
 __all__ = [
     "Chat",
-    "ChatGreeting",
     "chat_greeting",
     "chat_ui",
     "MarkdownStream",
