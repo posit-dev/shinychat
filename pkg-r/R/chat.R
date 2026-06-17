b21593b267daa17c0e827fbbda96b1cbddd22ee1
Garrick Aden-Buie <garrick@adenbuie.com>
2026-06-17 14:57:20 -0400

    r: document

--- pkg-r/R/chat.R
+++ pkg-r/R/chat.R
@@ -138,9 +138,9 @@ chat_greeting <- function(
 #' **`greeting_dismissed` input.** When the user dismisses the greeting,
 #' `input$<id>_greeting_dismissed` fires with a `Date.now()` timestamp. If the
 #' greeting is later cleared after being dismissed, the input resets to `NULL`.
-#' If you use [chat_mod_server()],
-#' you can access the `greeting_dismissed` reactive from the returned module
-#' value instead of the raw namespaced input string.
+#' If you use [chat_mod_server()], you can access the `greeting_dismissed`
+#' reactive from the returned module value instead of the raw namespaced input
+#' string.
 #'
 #' @param id The ID of the chat element
 #' @param ... Extra HTML attributes to include on the chat element
