8c9f093b7f5d233c05a8d8e8dfe40961f8c7253b
Carson <cpsievert1@gmail.com>
2026-08-19 18:33:14 -0500

    test(r): cover history save lookup by module id

--- pkg-r/tests/testthat/test-chat_history_integration.R
+++ pkg-r/tests/testthat/test-chat_history_integration.R
@@ -97,6 +97,44 @@ test_that("chat_server() history save persists an active conversation", {
   )
 })
 
+test_that("chat_server() history save finds a non-default controller", {
+  skip_if_not_installed("ellmer")
+
+  client <- mock_chat_client()
+  store <- InMemoryConversationStore$new()
+  chat_module <- NULL
+
+  shiny::testServer(
+    function(input, output, session) {
+      chat_module <<- chat_server(
+        "mychat",
+        client,
+        history = history_options(
+          store = store,
+          scope = "test-user",
+          title = NULL
+        ),
+        session = session
+      )
+    },
+    {
+      session$setInputs(mychat_history_browser_token = "browser-token")
+      ctrl <- get_session_chat_bookmark_info(
+        session,
+        "mychat.history-controller"
+      )
+      ctrl$record <- new_conversation_record("Saved conversation")
+
+      expect_false(is.null(ctrl$partition))
+      expect_identical(chat_module$history$save(), TRUE)
+      expect_equal(
+        store$get(ctrl$partition, ctrl$record$id)$title,
+        "Saved conversation"
+      )
+    }
+  )
+})
+
 test_that("chat_server() history save returns FALSE when history is disabled", {
   skip_if_not_installed("ellmer")
 
